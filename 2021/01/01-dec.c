df75240fe28c4422cdf5afed47d1b50f6a2c166b
Gabriel Mocanu <gabi.mocanu98@gmail.com>
2021-12-02 22:57:27 +0200

    Adding tasks for 01 december.
    Signed-off-by: Gabriel Mocanu <gabi.mocanu98@gmail.com>

---
+++ 2021/01/01-dec.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX_LEN 256
+
+int main() {
+
+    
+    int nr = 0;
+    int n;
+    FILE *fp;
+    char buffer[1000];
+    fp = fopen("input-1", "r");
+    if(fp == NULL) {
+        perror("Failed to open the file");
+        return 1;
+    }
+
+    fgets(buffer, MAX_LEN, fp);
+    buffer[strlen(buffer) - 1] = '\0';
+    n = atoi(buffer);
+
+    while(fgets(buffer, MAX_LEN, fp)) {
+        buffer[strlen(buffer) - 1] = '\0';
+        if(n < atoi(buffer)) {
+            nr++;
+        }
+        n = atoi(buffer);
+    }
+
+    printf("%d\n", nr);
+    fclose(fp);
+
+    return 0;
+}
---
+++ 2021/01/02-dec.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX_LEN 256
+
+
+int sum_three(int a, int b, int c) {
+    return a + b + c;
+}
+
+int main() {
+
+    
+    int nr = 0, i = 0;
+    FILE *fp;
+    char buffer[1000];
+    int v[2000];
+    int len;
+    fp = fopen("input-2", "r");
+    if(fp == NULL) {
+        perror("Failed to open the file");
+        return 1;
+    }
+
+    while(fgets(buffer, MAX_LEN, fp)) {
+        buffer[strlen(buffer) - 1] = '\0';
+        v[i] = atoi(buffer);
+        i++;
+    }
+    len = i;
+
+    for(i = 0; i < len - 3; i++) { 
+        if(sum_three(v[i], v[i+1], v[i+2]) < sum_three(v[i+1], v[i+2], v[i+3])) {
+            nr++;
+        }
+    }
+
+    printf("%d\n", nr);
+    fclose(fp);
+
+    return 0;
+}
